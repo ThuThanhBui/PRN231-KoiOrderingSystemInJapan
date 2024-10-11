c0f3c1b3a0681b296da1e87462aa6071accd6e04
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-10-11 14:18:21 +0700

    update ui and controller follow call api

---
+++ FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEMINJAPAN/KoiOrderingSystemInJapan.MVCWebApp/Controllers/ServicesController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KoiOrderingSystemInJapan.Data.Context;
+using KoiOrderingSystemInJapan.Data.Models;
+using ServiceEntity = KoiOrderingSystemInJapan.Data.Models;
+using KoiOrderingSystemInJapan.Common;
+using KoiOrderingSystemInJapan.Service.Base;
+using Newtonsoft.Json;
+
+
+namespace KoiOrderingSystemInJapan.MVCWebApp.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly HttpClient _http;
+
+        public ServicesController()
+        {
+        }
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Services"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                        if (result != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<List<ServiceEntity.Service>>(result.Data.ToString());
+                            return View(data);
+
+                        }
+                    }
+                }
+            }
+            return View(new List<ServiceEntity.Service>());
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Services/" + id))
+            //    {
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            var content = await response.Content.ReadAsStringAsync();
+            //            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+            //            if (result != null)
+            //            {
+            //                var data = JsonConvert.DeserializeObject<Service>(result.Data.ToString());
+            //                return View(data);
+
+            //            }
+            //        }
+            //    }
+            //}
+            //return View(new Service());
+            return View(id);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ServiceEntity.Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsJsonAsync(Const.APIEndPoint + "Services/", service))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                            if (result != null && result.Status == Const.SUCCESS_CREATE_CODE)
+                            {
+                            }
+                            else
+                            {
+                                return View(service);
+                            }
+                        }
+                    }
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Services/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                        if (result != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<ServiceEntity.Service>(result.Data.ToString());
+
+                            return View(data);
+                        }
+                    }
+                }
+            }
+            return NotFound();
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,PaymentAmount,PaymentDate,CreatedBy,CreatedDate,UpdatedBy,UpdatedDate,IsDeleted")] ServiceEntity.Service service)
+        {
+            if (id != service.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsJsonAsync(Const.APIEndPoint + "Services/", service))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                            if (result != null && result.Status == Const.SUCCESS_UPDATE_CODE)
+                            {
+                            }
+                            else
+                            {
+                                return View(service);
+                            }
+                        }
+                    }
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Services/Delete/5
+        //public async Task<IActionResult> Delete(Guid? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var service = await _context.Services
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (service == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(service);
+        //}
+
+        // POST: Services/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(Guid id)
+        //{
+        //    var service = await _context.Services.FindAsync(id);
+        //    if (service != null)
+        //    {
+        //        _context.Services.Remove(service);
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        //private bool ServiceExists(Guid id)
+        //{
+        //    return _context.Services.Any(e => e.Id == id);
+        //}
+    }
+}
