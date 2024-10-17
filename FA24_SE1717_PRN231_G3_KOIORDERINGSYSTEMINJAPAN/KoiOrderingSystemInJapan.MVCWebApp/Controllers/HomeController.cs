3f3129518cb379cf39c79f10744d51bad128c1e1
khoipnm <pkhoi2015@gmail.com>
2024-10-17 15:36:46 +0700

    update home UI

--- FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEMINJAPAN/KoiOrderingSystemInJapan.MVCWebApp/Controllers/HomeController.cs
+++ FA24_SE1717_PRN231_G3_KOIORDERINGSYSTEMINJAPAN/KoiOrderingSystemInJapan.MVCWebApp/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
+using KoiOrderingSystemInJapan.Common;
+using KoiOrderingSystemInJapan.Data.Models;
+using KoiOrderingSystemInJapan.Service.Base;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace KoiOrderingSystemInJapan.MVCWebApp.Controllers
 {
@@ -11,9 +15,32 @@ namespace KoiOrderingSystemInJapan.MVCWebApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        // GET: Travels
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var data = await GetTravelsAsync();
+
+            return View(data);
+        }
+        private async Task<List<Travel>> GetTravelsAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndPoint + "Travels"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+                        if (result != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<List<Travel>>(result.Data.ToString());
+                            return data;
+                        }
+                    }
+                }
+            }
+            return new List<Travel>();
         }
 
         public IActionResult Privacy()
